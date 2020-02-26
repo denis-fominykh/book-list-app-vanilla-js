13e8601ebc02021577b9a6d78d6ab5e8fd4235ed
denis-fominykh <fominykhdenis@gmail.com>
2020-02-26 23:56:46 +0200

    adding user book to the list

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
 import { UI } from './ui';
+import { Book } from './book';
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+document.querySelector('#book-form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  const book = new Book(title, author, isbn);
+
+  UI.addBookToList(book);
+});
